d8b48062e54a013afdf88813839cca6b9c8b035f
Sahana BalajiKumar <86933110+sahanabalajikumar@users.noreply.github.com>
2021-08-23 00:15:30 +0000

    added new file

---
+++ MergeLists.py
@@ -0,0 +1,10 @@
+
+def mergesortarray(list1, list2, m1, m2):
+  kamnaisgaga = []
+  for i in range(m1):
+    kamnaisgaga.append(list1[i])
+  for i in range(m2):
+    kamnaisgaga.append(list2[i])
+  print(kamnaisgaga)
+
+mergesortarray([1, 2, 3], [1, 2, 3, 4, 5], 2, 3)
